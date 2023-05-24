e8734e2aef6197db4afe6f0eb140fec014c96f74
olvera93 <olvera9302@outlook.com>
2023-05-24 12:30:11 -0600

    [ADD] Pause video

--- lib/presentation/widgets/shared/video_scrollable_view.dart
+++ lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 
 class VideoScrolableView extends StatelessWidget {
@@ -23,6 +24,11 @@ class VideoScrolableView extends StatelessWidget {
         return Stack(
           children: [
             // Video Player
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                videoUrl: videoPost.videoUrl, 
+                caption: videoPost.caption)
+            ),
 
             // Buttons
             Positioned(
